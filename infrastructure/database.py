0e27f2671996b1912182ed5c96f0472627084809
KareemOsama15 <karim.osama.saleh@gmail.com>
2026-04-25 22:35:35 +0300

    Enhance Database and Repository classes with CRUD functionality
    - Updated Database class to include methods for creating, retrieving, listing, updating, and deleting records.
    - Modified Repository class to utilize the new Database methods and added a method to resolve table names based on model attributes.
    - Introduced type hints for better code clarity and maintainability.

--- infrastructure/database.py
+++ infrastructure/database.py
@@ -1,10 +1,46 @@
 import sqlite3
-from typing import Any
+from typing import Any, List, Dict
 
 
 class Database:
     """Database class for the application"""
 
-    def __init__(self, db_path):
+    def __init__(self, db_path: str, table_name: str):
         self.conn = sqlite3.connect(db_path)
+        self.conn.row_factory = sqlite3.Row
         self.cursor = self.conn.cursor()
+        self.table_name = table_name
+
+    def create_record(self, data: Dict[str, Any]) -> int:
+        """Create a new record for a table in the database"""
+        self.cursor.execute(
+            f"INSERT INTO {self.table_name} ({', '.join(data.keys())}) VALUES ({', '.join(data.values())})"
+        )
+        self.conn.commit()
+        return self.cursor.lastrowid
+
+    def get_record(self, id: int) -> Dict[str, Any] | None:
+        """Get a record from a table in the database"""
+        self.cursor.execute(f"SELECT * FROM {self.table_name} WHERE id = {id}")
+        return self.cursor.fetchone()
+
+    def list_records(self) -> List[Dict[str, Any]] | None:
+        """List all records from a table in the database"""
+        self.cursor.execute(f"SELECT * FROM {self.table_name}")
+        return self.cursor.fetchall()
+
+    def update_record(self, id: int, data: Dict[str, Any]) -> None:
+        """Update a record in a table in the database"""
+        self.cursor.execute(
+            f"UPDATE {self.table_name} SET {', '.join([f'{key} = {value}' for key, value in data.items()])} WHERE id = {id}"
+        )
+        self.conn.commit()
+
+    def delete_record(self, id: int) -> None:
+        """Delete a record from a table in the database"""
+        self.cursor.execute(f"DELETE FROM {self.table_name} WHERE id = {id}")
+        self.conn.commit()
+
+    def close(self) -> None:
+        """Close the connection to the database"""
+        self.conn.close()
--- infrastructure/repository.py
+++ infrastructure/repository.py
@@ -1,5 +1,5 @@
 from infrastructure.database import Database
-from typing import Any
+from typing import Any, List, Dict
 
 
 class Repository:
@@ -11,25 +11,32 @@ class Repository:
     """
 
     def __init__(self, model: Any, db_path="app.db"):
-        self.database = Database(db_path)
-        self.model = model
+        self.table_name = self.__resolve_table_name(model)
+        self.database = Database(db_path, self.table_name)
 
-    def create(self, data: Any):
+    def create(self, data: Dict[str, Any]) -> None:
         """Create a new record in the database"""
-        pass
+        return self.database.create_record(data=data)
 
-    def get(self, id: int):
+    def get(self, id: int) -> Dict[str, Any] | None:
         """Get a record from the database"""
-        pass
+        return self.database.get_record(id=id)
 
-    def list(self):
+    def list(self) -> List[Dict[str, Any]] | None:
         """List all records from the database"""
-        pass
+        return self.database.list_records()
 
-    def update(self, id: int, data: Any):
+    def update(self, id: int, data: Dict[str, Any]) -> None:
         """Update a record in the database"""
-        pass
+        self.database.update_record(id=id, data=data)
 
-    def delete(self, id: int):
+    def delete(self, id: int) -> None:
         """Delete a record from the database"""
-        pass
+        self.database.delete_record(id=id)
+
+    def __resolve_table_name(self, model: Any) -> str:
+        """Resolve the table name for the model"""
+        if hasattr(model, "__table_name__"):
+            return model.__table_name__
+        else:
+            return f"{model.__name__.lower()}s"
